db8d6018ee1dc7c14d7dcad3a04c5a16586ce193
eflores <eflores@digepres.gob.do>
2018-06-22 15:50:04 -0400

    adding routing

--- CrudAngular/client/angular/src/app/app.module.ts
+++ CrudAngular/client/angular/src/app/app.module.ts
@@ -1,19 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { VehicleComponent } from './components/vehicle/vehicle.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    VehicleComponent
+    VehicleComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'vehicles', pathMatch: 'full' },
+      { path: 'vehicles/new', component: VehicleComponent },
+      { path: 'home', component: HomeComponent },
+      { path: '**', redirectTo: 'home' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
