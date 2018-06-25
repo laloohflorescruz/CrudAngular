6c44d96a6498dfcc523360ee67586099bd14cdec
Lalo Flores Cruz <eflores@crudcreativo.com>
2018-06-24 21:40:58 -0400

    routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,14 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { VehicleformComponent } from './components/vehicleform/vehicleform.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms'; 
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'vehicles/new', component: VehicleformComponent },
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +22,9 @@ import { VehicleformComponent } from './components/vehicleform/vehicleform.compo
     VehicleformComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
