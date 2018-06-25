8403c65e6620910648c896cb638731148cca56d2
Lalo Flores Cruz <eflores@crudcreativo.com>
2018-06-24 21:34:08 -0400

    home, nav and vehicle component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { VehicleformComponent } from './components/vehicleform/vehicleform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    VehicleformComponent
   ],
   imports: [
     BrowserModule
